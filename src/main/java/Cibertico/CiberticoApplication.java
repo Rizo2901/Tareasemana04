4cabf87fa46cfc1e4a7b5cdb5749828a3ff20f17
Rizo2901 <114025356+Rizo2901@users.noreply.github.com>
2023-10-02 17:13:03 -0600

    CiberTico, Tarea de semana04

---
+++ src/main/java/Cibertico/CiberticoApplication.java
@@ -0,0 +1,13 @@
+package Cibertico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiberticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiberticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Cibertico/CiberticoApplicationTests.java
@@ -0,0 +1,13 @@
+package Cibertico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiberticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
